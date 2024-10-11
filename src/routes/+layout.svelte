3a051658782b7ea6bc3916968f247fd4ee6fc3ba
jpcamposgda <jpcamposgda@gmail.com>
2024-10-11 18:52:01 -0400

    feat: Create AppBar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
 	import css from 'highlight.js/lib/languages/css';
 	import javascript from 'highlight.js/lib/languages/javascript';
 	import typescript from 'highlight.js/lib/languages/typescript';
+	import logo from '$lib/assets/logo.png';
 
 	hljs.registerLanguage('xml', xml); // for HTML
 	hljs.registerLanguage('css', css);
@@ -20,44 +21,98 @@
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
+	import { MagnifyingGlass, ShoppingCart, UserCircle } from 'svelte-heros-v2';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	let search = '';
+
+	function doSearch() {
+		console.log(search);
+	}
 </script>
 
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
+
+		<AppBar
+			gridColumns="grid-cols-3"
+			slotDefault="place-self-center"
+			slotTrail="place-content-end"
+			background="bg-black "
+		>
+			<div slot="lead" class="w-1/3">
+				<div class="flex-none inline-flex items-center h-12 md:h-16">
+					<img src={logo} alt="Gatto Rosa" class="h-full w-auto" />
+				</div>
+			</div>
+
+			<div slot="trail" class="w-2/3 flex">
+				<div class="relative flex">
+					<input
+						class="w- bg-transparent placeholder:text-slate-400 text-white text-sm border border-primary rounded-l-md py-2 transition duration-300 ease focus:outline-none focus:border-primary hover:border-primary shadow-sm focus:shadow"
+						placeholder="Digite o que você procura"
+					/>
+					<button
+						class="flex items-center justify-center rounded-r-md bg-transparent p-2 border border-primary border-l-0 text-sm text-white transition-all shadow-sm hover:shadow focus:bg-primary focus:shadow-none active:bg-primary hover:bg-primary active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+						type="button"
+					>
+						<MagnifyingGlass class="h-5 w-5 text-primary hover:text-black" />
+					</button>
+				</div>
+				<button
+					class="flex items-center justify-center rounded-full bg-transparent p-2 transition-all shadow-sm hover:shadow focus:bg-white focus:shadow-none active:bg-white hover:bg-white active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+					type="button"
 				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
+					<UserCircle size="30" class="text-primary" />
+				</button>
+
+				<button
+					class="flex items-center justify-center rounded-full bg-transparent p-2 transition-all shadow-sm hover:shadow focus:bg-white focus:shadow-none active:bg-white hover:bg-white active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+					type="button"
 				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
+					<ShoppingCart size="30" class="text-primary" />
+				</button>
+			</div>
 		</AppBar>
+		<!-- <AppBar background="bg-black " style="width: 100%;">
+			<div slot="lead" class="flex items-center justify-between">
+				<div>
+					<div class="flex-none inline-flex items-center h-12 md:h-16">
+						<img src={logo} alt="Gatto Rosa" class="h-full w-auto" />
+					</div>
+					<div class=" max-w-sm min-w-[200px] flex justify-around">
+						<div class="relative flex">
+							<input
+								class="w-full bg-transparent placeholder:text-slate-400 text-white text-sm border border-primary rounded-l-md pl-10 pr-0 py-2 transition duration-300 ease focus:outline-none focus:border-primary hover:border-primary shadow-sm focus:shadow"
+								placeholder="Digite o que você procura"
+							/>
+							<button
+								class="flex items-center justify-center rounded-r-md bg-transparent p-2 border border-primary border-l-0 text-sm text-white transition-all shadow-sm hover:shadow focus:bg-primary focus:shadow-none active:bg-primary hover:bg-primary active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+								type="button"
+							>
+								<MagnifyingGlass class="h-5 w-5" />
+							</button>
+						</div>
+
+						<button
+							class="flex items-center justify-center rounded-r-md bg-transparent p-2 border border-primary border-l-0 text-sm text-white transition-all shadow-sm hover:shadow focus:bg-primary focus:shadow-none active:bg-primary hover:bg-primary active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+							type="button"
+						>
+							<UserCircle class="h-5 w-5" />
+						</button>
+
+						<button
+							class="flex items-center justify-center rounded-r-md bg-transparent p-2 border border-primary border-l-0 text-sm text-white transition-all shadow-sm hover:shadow focus:bg-primary focus:shadow-none active:bg-primary hover:bg-primary active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+							type="button"
+						>
+							<ShoppingCart class="h-5 w-5" />
+						</button>
+					</div>
+				</div>
+			</div>
+		</AppBar> -->
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<slot />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,46 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/tw-plugin'
+import { skeleton } from '@skeletonlabs/tw-plugin';
 
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Montserrat', 'sans-serif']
+			},
+			colors: {
+				primary: {
+					DEFAULT: '#da9a39',
+					dark: '#ac7c34',
+					50: '#FDFBF6',
+					100: '#FAF2E5',
+					200: '#F5E5CB',
+					300: '#F0D5AD',
+					400: '#EAC68F',
+					500: '#E4B671',
+					600: '#E0A957',
+					700: '#DA9A39',
+					800: '#9B681D',
+					900: '#4D340E',
+					950: '#271A07'
+				}
+			}
+		}
 	},
 	plugins: [
 		forms,
 		typography,
 		skeleton({
 			themes: {
-				preset: [
-					{
-						name: 'gold-nouveau',
-						enhancements: true,
-					},
-				],
-			},
-		}),
-	],
+				preset: [] // Desativar temas padrão do Skeleton
+			}
+		})
+	]
 } satisfies Config;
