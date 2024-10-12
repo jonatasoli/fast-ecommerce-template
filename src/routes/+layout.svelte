45d5a0f31382a0afd447e04a428adece0254f6f0
jpcamposgda <jpcamposgda@gmail.com>
2024-10-12 11:56:54 -0400

    feat: mobile layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -32,35 +32,57 @@
 </script>
 
 <!-- App Shell -->
+
 <AppShell>
 	<svelte:fragment slot="header">
-		<!-- App Bar -->
-
 		<AppBar
 			gridColumns="grid-cols-3"
 			slotDefault="place-self-center"
 			slotTrail="place-content-end"
-			background="bg-black "
+			background="bg-black"
+			padding="p-2"
+			class="flex justify-center items-center"
 		>
-			<div slot="lead" class="w-1/3">
-				<div class="flex-none inline-flex items-center h-12 md:h-16">
-					<img src={logo} alt="Gatto Rosa" class="h-full w-auto" />
+			<div slot="lead">
+				<div
+					class="flex-none inline-flex items-center justify-center h-12 md:h-16 w-[150px] sm:w-[250px]"
+				>
+					<img src={logo} alt="Logo" class="h-full w-auto" />
+				</div>
+				<div class="flex w-screen items-center justify-center">
+					<div class="h-10 flex mx-auto sm:hidden w-[300px]">
+						<input
+							class="w-full bg-transparent placeholder:text-slate-400 text-white text-sm border border-primary rounded-l-lg py-2 transition duration-300 ease focus:outline-none focus:ring-0 focus:border-primary hover:border-primary shadow-sm focus:shadow"
+							placeholder="Digite o que você procura"
+						/>
+						<button
+							class="flex items-center justify-center rounded-r-lg bg-transparent p-2 border border-primary border-l-0 text-sm text-white transition-all shadow-sm hover:shadow focus:bg-primary focus:shadow-none active:bg-primary hover:bg-primary active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+							type="button"
+						>
+							<MagnifyingGlass class="h-5 w-5 text-primary hover:text-black" />
+						</button>
+					</div>
 				</div>
 			</div>
-
-			<div slot="trail" class="w-2/3 flex">
-				<div class="relative flex">
+			<div
+				slot="trail"
+				class="flex space-x-4 items-center justify-center xs:absolute xs:top-0 sm:relative"
+			>
+				<div
+					class="relative h-10 w-full md:w-[400px] max-w-full md:max-w-[400px] sm:w-[200px] hidden sm:flex"
+				>
 					<input
-						class="w- bg-transparent placeholder:text-slate-400 text-white text-sm border border-primary rounded-l-md py-2 transition duration-300 ease focus:outline-none focus:border-primary hover:border-primary shadow-sm focus:shadow"
+						class="w-full bg-transparent placeholder:text-slate-400 text-white text-sm border border-primary rounded-l-lg py-2 transition duration-300 ease focus:outline-none focus:ring-0 focus:border-primary hover:border-primary shadow-sm focus:shadow"
 						placeholder="Digite o que você procura"
 					/>
 					<button
-						class="flex items-center justify-center rounded-r-md bg-transparent p-2 border border-primary border-l-0 text-sm text-white transition-all shadow-sm hover:shadow focus:bg-primary focus:shadow-none active:bg-primary hover:bg-primary active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+						class="flex items-center justify-center rounded-r-lg bg-transparent p-2 border border-primary border-l-0 text-sm text-white transition-all shadow-sm hover:shadow focus:bg-primary focus:shadow-none active:bg-primary hover:bg-primary active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
 						type="button"
 					>
 						<MagnifyingGlass class="h-5 w-5 text-primary hover:text-black" />
 					</button>
 				</div>
+
 				<button
 					class="flex items-center justify-center rounded-full bg-transparent p-2 transition-all shadow-sm hover:shadow focus:bg-white focus:shadow-none active:bg-white hover:bg-white active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
 					type="button"
@@ -76,44 +98,18 @@
 				</button>
 			</div>
 		</AppBar>
-		<!-- <AppBar background="bg-black " style="width: 100%;">
-			<div slot="lead" class="flex items-center justify-between">
-				<div>
-					<div class="flex-none inline-flex items-center h-12 md:h-16">
-						<img src={logo} alt="Gatto Rosa" class="h-full w-auto" />
-					</div>
-					<div class=" max-w-sm min-w-[200px] flex justify-around">
-						<div class="relative flex">
-							<input
-								class="w-full bg-transparent placeholder:text-slate-400 text-white text-sm border border-primary rounded-l-md pl-10 pr-0 py-2 transition duration-300 ease focus:outline-none focus:border-primary hover:border-primary shadow-sm focus:shadow"
-								placeholder="Digite o que você procura"
-							/>
-							<button
-								class="flex items-center justify-center rounded-r-md bg-transparent p-2 border border-primary border-l-0 text-sm text-white transition-all shadow-sm hover:shadow focus:bg-primary focus:shadow-none active:bg-primary hover:bg-primary active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-								type="button"
-							>
-								<MagnifyingGlass class="h-5 w-5" />
-							</button>
-						</div>
-
-						<button
-							class="flex items-center justify-center rounded-r-md bg-transparent p-2 border border-primary border-l-0 text-sm text-white transition-all shadow-sm hover:shadow focus:bg-primary focus:shadow-none active:bg-primary hover:bg-primary active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-							type="button"
-						>
-							<UserCircle class="h-5 w-5" />
-						</button>
-
-						<button
-							class="flex items-center justify-center rounded-r-md bg-transparent p-2 border border-primary border-l-0 text-sm text-white transition-all shadow-sm hover:shadow focus:bg-primary focus:shadow-none active:bg-primary hover:bg-primary active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-							type="button"
-						>
-							<ShoppingCart class="h-5 w-5" />
-						</button>
-					</div>
-				</div>
-			</div>
-		</AppBar> -->
 	</svelte:fragment>
-	<!-- Page Route Content -->
+
+	<svelte:fragment slot="sidebarLeft">
+		<!-- Hidden below Tailwind's large breakpoint -->
+		<div id="sidebar-left" class="hidden lg:block">Sidebar</div>
+	</svelte:fragment>
+
+	<svelte:fragment slot="pageHeader">
+		<AppBar>pageHeader</AppBar>
+	</svelte:fragment>
+
 	<slot />
+
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
 </AppShell>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,24 @@ export default {
 		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
 	theme: {
+		screens: {
+			xs: '0px',
+
+			sm: '640px',
+			// => @media (min-width: 640px) { ... }
+
+			md: '804px',
+			// => @media (min-width: 804px) { ... }
+
+			lg: '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			xl: '1280px',
+			// => @media (min-width: 1280px) { ... }
+
+			'2xl': '1536px'
+			// => @media (min-width: 1536px) { ... }
+		},
 		extend: {
 			fontFamily: {
 				sans: ['Montserrat', 'sans-serif']
